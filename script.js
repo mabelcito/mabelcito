2cca832e83f899d49175de708ad9945b8cf814b0
mabelcito <matthewfranco56@gmail.com>
2021-02-10 09:01:13 -0500

    mi primer commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var typed = new Typed(".typing", {
+    strings: ["Sitio Web", "Portafolio"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed('.prueba',{
+    strings: [
+        "Mabelcito",
+        "mabelcito.site"
+    ],
+typeSpeed: 60,
+backSpeed: 50,
+loop: 50
+});
+var typed = new Typed('.prueba1',{
+    strings: [
+        "Programador",
+        "Pre-Diseñador",
+        "Esclavo"
+    ],
+typeSpeed: 60,
+backSpeed: 50,
+loop: 50
+});
