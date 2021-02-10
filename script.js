4e3eef9f64f3f0893aee22507aeb52347bbe5cc4
mabelcito <matthewfranco56@gmail.com>
2021-02-10 10:37:32 -0500

    segundo

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".typing", {
-    strings: ["Sitio Web", "Portafolio"],
-    typeSpeed: 100,
-    backSpeed: 60,
+    strings: ["Sitio Web.", "Portafolio."],
+    typeSpeed: 40,
+    backSpeed: 45,
     loop: true
 });
 var typed = new Typed('.prueba',{
@@ -9,17 +9,41 @@ var typed = new Typed('.prueba',{
         "Mabelcito",
         "mabelcito.site"
     ],
-typeSpeed: 60,
-backSpeed: 50,
+typeSpeed: 40,
+backSpeed: 45,
 loop: 50
 });
 var typed = new Typed('.prueba1',{
     strings: [
-        "Programador",
-        "Pre-Diseñador",
-        "Esclavo"
+        "Programador en proceso.",
+        "Pre-Diseñador.",
+        "Un buen esclavo."
+    ],
+typeSpeed: 40,
+backSpeed: 45,
+loop: 50
+});
+var typed = new Typed('.prueba2',{
+    strings: [
+        "Diseño"
     ],
 typeSpeed: 60,
-backSpeed: 50,
+backSpeed: 70,
+loop: 50
+});
+var typed = new Typed('.prueba3',{
+    strings: [
+        "Desarrollo"
+    ],
+typeSpeed: 50,
+backSpeed: 30,
+loop: 50
+});
+var typed = new Typed('.prueba4',{
+    strings: [
+        "Seguridad"
+    ],
+typeSpeed: 60,
+backSpeed: 30,
 loop: 50
 });
